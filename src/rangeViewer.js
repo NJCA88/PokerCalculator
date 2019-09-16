8f0bca208877b2332729ce3349576b08c179289b
Chris Atwood <coachchris289@gmail.com>
2019-09-16 15:26:28 -0700

    impliment visualized range viewer

--- src/rangeViewer.js
+++ src/rangeViewer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './rangeview.css'
 
 // const RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
 const RANKS = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
@@ -7,24 +8,26 @@ const SUITS = ['c', 's', 'h', 'd'];
  class RangeViewer extends React.Component {
     constructor(props){
         super(props)
-        this.state={selected:[]}
-        this.generateRangeView()
+        this.state={selected: ""  }
+        // this.generateRangeView()
     }
     handleClick = (input) =>{
-        let newSelected = this.state.selected.dup.push(input)
-        this.setState({selected: newSelected})
+        // let newSelected = this.state.selected.dup[input] = true
+        // console.log(new)
+        if (!this.state[input]){
+            this.setState({[input]: true}, ()=>console.log(this.state))
+        } else{
+            this.setState({[input]: false}, ()=>console.log(this.state))
+        }
     }
     generateRangeView = () =>{
         let buttonLabelStrings = []
-
         for (let first=0; first< RANKS.length; first++){
-            //for each column
             let suited = 'o'
             for (let last = 0; last < RANKS.length; last++) {
                 if (first == last){
                     suited = 's'
                 }
-                // buttonLabelStrings.push(RANKS[first] + RANKS[last] + suited)
                 if (first === last){
                     buttonLabelStrings.push(RANKS[first] + RANKS[last])
                 } else{
@@ -34,11 +37,21 @@ const SUITS = ['c', 's', 'h', 'd'];
         }
         console.log("buttons are: ", buttonLabelStrings)
 
+        this.rangeButtons = buttonLabelStrings.map(str =>{
+
+            return (
+                <button onClick={e=>this.handleClick(str)} className={ this.state[str] ? "selected" : 'unselected' }> {str}</button>
+            )
+        })
+
         
     }
     render = () =>{
+        this.generateRangeView()
         return(
-            <div>RANGE VIEWER</div>
+            <div className='range-view-container'>
+                {this.rangeButtons}
+            </div>
         )
     }
 }
